df68dd63f0208f4ebc07386370248b8c6e4c610d
25prajwal <94011967+25prajwal@users.noreply.github.com>
2023-03-28 20:16:39 +0530

    add Xpshort function

--- main.py
+++ main.py
@@ -8,6 +8,30 @@ app = Flask(__name__)
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@app.route("/xpshort/<path:url>")
+def xpshort(url):
+    import time
+    import requests
+    from bs4 import BeautifulSoup 
+    client = requests.session()
+    DOMAIN = "https://xpshort.com/"
+    url = url[:-1] if url[-1] == '/' else url
+    code = url.split("/")[-1]
+    final_url = f"{DOMAIN}/{code}"
+    ref = "https://www.jankarihoga.com/"
+    h = {"referer": ref}
+    resp = client.get(final_url,headers=h)
+    soup = BeautifulSoup(resp.content, "html.parser")
+    inputs = soup.find_all("input")
+    data = { input.get('name'): input.get('value') for input in inputs }
+    h = { "x-requested-with": "XMLHttpRequest" }  
+    time.sleep(8)
+    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+    try:
+        return r.json()['url']
+    except: 
+        return "SomeThing went wrong"
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
