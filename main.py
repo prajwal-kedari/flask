fa98f1484a923c8ead6b350695da47150cc0b7f1
25prajwal <94011967+25prajwal@users.noreply.github.com>
2023-04-13 00:46:50 +0530

    Update

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ def xpshort(url):
     url = url[:-1] if url[-1] == '/' else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    #ref = "https://b.finsurances.co/"
-    ref = "https://www.jankarihoga.com/"
+    ref = "https://b.finsurances.co/"
+    #ref = "https://www.jankarihoga.com/"
     h = {"referer": ref}
     resp = client.get(final_url,headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
