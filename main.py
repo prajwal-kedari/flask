e6797d3faca46b6f128227719d994fee74895636
prajwal-kedari <96010758+prajwal-kedari@users.noreply.github.com>
2023-05-11 00:03:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def xpshort(url):
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
     ref = "https://go.finsurances.co/"
-    #ref = "https://www.jankarihoga.com/"
+    #ref = "https://go.ecowas.in"
     h = {"referer": ref}
     resp = client.get(final_url,headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
